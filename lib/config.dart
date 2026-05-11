58b0aaee2c1ad81b7168e9a64540e0070fbe000a
RAMDHAN-MQ <ramdhanqolbi@gmail.com>
2026-05-11 23:09:49 +0700

    config

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  // static const String baseUrl = "http://192.168.1.141:8000";
-  static const String baseUrl = "http://10.103.127.140:8000";
+  static const String baseUrl = "http://192.168.1.64:8000";
+  // static const String baseUrl = "http://10.103.127.140:8000";
 }
